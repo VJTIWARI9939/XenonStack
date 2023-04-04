559474887cf36b67bc68b110ab3d75dc485630d7
Vijay Tiwari <116339934+VJTIWARI9939@users.noreply.github.com>
2023-04-04 22:23:58 +0530

    Add files via upload

---
+++ ParulManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class ParulManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParulManagementSystemApplication.class, args);
+	}
+
+}
